ec9174d3f548ed43e838ea417949ae14e5774a19
marcogarciakoch <marcogarciakoch@gmail.com>
2022-05-10 11:42:56 +0200

    first-commit-react-practice-0905

---
+++ students/marco-garcia-koch/react-practice/react-app-0905/src/components/Nav/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+
+function Nav () {
+
+
+    return (
+        <nav className='nav'>MENU DE NAVEGACION</nav>
+    )
+}
+
+export default Nav;
---
+++ students/marco-garcia-koch/react-practice/react-app-0905/src/components/SideBar/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+
+function SideBar () {
+
+
+    return (
+        <div className='side-bar'>ESTE ES EL SIDE BAR DE LA PAGINA</div>
+    )
+}
+
+
+export default SideBar;
