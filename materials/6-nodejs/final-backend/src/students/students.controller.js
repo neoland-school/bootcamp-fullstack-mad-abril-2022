c6bba9e589dce60b7b01552d6fc9d5cc77697052
Alex González <alejandrogsa@gmail.com>
2022-06-06 13:35:00 +0200

    manage id

--- materials/6-nodejs/final-backend/src/students/students.controller.js
+++ materials/6-nodejs/final-backend/src/students/students.controller.js
@@ -1,7 +1,7 @@
 import { ObjectId } from "mongodb";
 
 // GET ALL
-export const getAllStudentsCtrl = async (req,res) => {
+export const getAllStudentsCtrl = async (req, res) => {
     // ir a BBDD y devolver TODOS los estudiantes
     const db = req.app.locals.ddbbClient.db('demo'); // cojo la BBDD
     const col = db.collection('students'); // cojo la collection
@@ -9,22 +9,27 @@ export const getAllStudentsCtrl = async (req,res) => {
     res.json(students); // devuelvo el resultado al cliente
 }
 
-export const createStudentCtrl = async (req,res) => {
+export const createStudentCtrl = async (req, res) => {
     const db = req.app.locals.ddbbClient.db('demo'); // cojo la BBDD
     const col = db.collection('students'); // cojo la collection
     const r = await col.insertOne(req.body); // aqui falta VALIDAR el body
-    res.json({id: r.insertedId}); // devuelvo el ID insertado para que el cliente sepa
+    res.json({ id: r.insertedId }); // devuelvo el ID insertado para que el cliente sepa
 }
 
-export const getStudentByIdCtrl = async (req,res) => {
-    const {id} = req.params;
+export const getStudentByIdCtrl = async (req, res) => {
+    const { id } = req.params;
     const db = req.app.locals.ddbbClient.db('demo'); // cojo la BBDD
     const col = db.collection('students'); // cojo la collection
-    const o_id = ObjectId(id); // genero un ObjectId de MongoDB. Controlar el pete del ID
-    const student = await col.findOne(o_id);
-    if(student === null ){
-        res.status(404).json({error: 'No existe ese usuario'});
+    if (id.length === 12 || id.length === 24 ) {
+        const o_id = ObjectId(id); // genero un ObjectId de MongoDB. Controlar el pete del ID
+        const student = await col.findOne(o_id);
+        if (student === null) {
+            res.status(404).json({ error: 'No existe ese usuario' });
+        } else {
+            res.json(student);
+        }
     }else{
-        res.json(student);
+        res.status(400).json({ error: 'El ID no tiene el formato correcto'});
     }
+
 }
